d18dbc3dbe8efd97047c3178489771344a5a11c8
Brandon Fosso <brandon.fosso+github@gmail.com>
2015-08-18 21:17:37 -0500

    Author test.py, a place to experiment.

---
+++ test.py
@@ -0,0 +1,10 @@
+# A place for me to play
+
+# Character space:
+alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+plain = "TEST" # Plain text as a string
+
+# print plain[2]
+# print alph.index('A')
+print alph.index(plain[1])
