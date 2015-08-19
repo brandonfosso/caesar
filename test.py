6b30bf52adcabb59950c676d0059ac404676cd26
Brandon Fosso <brandon.fosso+github@gmail.com>
2015-08-18 22:20:19 -0500

    Clean up comments from debugging.

--- test.py
+++ test.py
@@ -3,9 +3,9 @@
 # Character space:
 alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-plain = "TEST, HAHA!" # Plain text as a string
+plain = "ABC" # Plain text as a string
 
-shift = 10 # Standard shift
+shift = 1 # Standard shift
 
 cipher = "" # Cipher text starts as empty string
 
@@ -18,17 +18,7 @@ for i in plain:
 
 	# print cipher # Watch it being built up 
 
-# print plain[2]
-# print alph.index('A')
-# print alph.index(plain[1])
-# print plain, shift
-# print alph[29%len(alph)]
-
 # Nice output:
 print "Plain Text:", plain
 print "Shift:", shift
-print "Cipher Text", cipher
-
-
-
-# Result should be WHVW, DOCD
+print "Cipher Text:", cipher
