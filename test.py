2e55df7033dfcaca412613414e8ec0f0531d7738
Brandon Fosso <brandon.fosso+github@gmail.com>
2015-08-18 21:25:08 -0500

    Add for loop doing shift of 0.

--- test.py
+++ test.py
@@ -5,6 +5,15 @@ alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
 
 plain = "TEST" # Plain text as a string
 
+shift = 3 # Standard shift
+
+cipher = "" # Cipher text starts as empty string
+
+for i in plain:
+	cipher += i
+	print cipher 
+
 # print plain[2]
 # print alph.index('A')
-print alph.index(plain[1])
+# print alph.index(plain[1])
+# print plain, shift
