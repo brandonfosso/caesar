e5449cf33b875c8a34538605cb071b0d8842b4e5
Brandon Fosso <brandon.fosso+github@gmail.com>
2015-08-18 21:49:38 -0500

    Add modulo to make shift properly circular.

--- test.py
+++ test.py
@@ -5,18 +5,25 @@ alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
 
 plain = "TEST" # Plain text as a string
 
-shift = 3 # Standard shift
+shift = 10 # Standard shift
 
 cipher = "" # Cipher text starts as empty string
 
 for i in plain:
-	cipher += alph[alph.index(i) + shift]
-	print cipher 
+	cipher += alph[(alph.index(i) + shift) % len(alph)] # Determine character index, add shift, determine new character
+	# print cipher 
 
 # print plain[2]
 # print alph.index('A')
 # print alph.index(plain[1])
 # print plain, shift
+# print alph[29%len(alph)]
+
+# Nice output:
+print "Plain Text:", plain
+print "Shift:", shift
+print "Cipher Text", cipher
 
 
-# Result should be WHVW
+
+# Result should be WHVW, DOCD
