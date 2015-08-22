8e2263c9e455d2819aaf7e06111ad9222ddb529d
Brandon Fosso <brandon.fosso+github@gmail.com>
2015-08-22 14:32:16 -0500

    Add small letter alphabet.

--- test.py
+++ test.py
@@ -5,7 +5,8 @@
 import time
 
 # Character space:
-alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+small_alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+big_alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 plain = raw_input('Enter your plain text: ') # User Plaintext
 
